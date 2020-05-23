5271e1a1915ede6605dbca148f0120402f193700
tessiemcgough <mmcgough@chicagobooth.edu>
2020-05-23 22:56:39 +0000

    Added final changes

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,6 +5,10 @@ class UsersController < ApplicationController
     render({:template => "photogram_templates/users.html.erb"})
   end 
 
+  def list_photos 
+    @photos = User.find_by_sql("SELECT * FROM Photos ORDER BY created_at DESC")
+    render({:template => "photogram_templates/photos.html.erb"})
+  end 
   def user_info
     name = params.fetch("username")
     @users = User.where(:Username => name).at(0)
@@ -12,12 +16,31 @@ class UsersController < ApplicationController
     render({:template => "photogram_templates/user_info.html.erb"})
   end
 
-  def create_row
+  def create_user_record
     u = User.new
     u.username = params.fetch("new_user")
     u.save 
     redirect_to("/users/"+ u.username)
   end
+
+  def create_photo_record
+    photo = Photo.new
+    photo.image = params.fetch("image_src")
+    photo.caption = params.fetch("caption")
+    photo.owner_id = params.fetch("photo_author")
+    photo.save 
+    redirect_to("/photos/" + photo.id.to_s)
+  end 
+
+  def create_comment_record
+    comment = Comment.new
+    comment.photo_id = params.fetch("photo_id")
+    comment.body = params.fetch("comment")
+    comment.author_id = params.fetch("author_id")
+    comment.save
+    redirect_to("/photos/" + comment.photo_id.to_s)
+  
+  end 
 
   def update_row
     user_id = params.fetch("user_id_number")
@@ -33,7 +56,13 @@ class UsersController < ApplicationController
   def photo_info 
     photo_id = params.fetch("photo_id")
     @photo_info = Photo.where(:id => photo_id).at(0)
+    
+    today_date = Date.today
+    created_date = @photo_info.created_at
+    @months_btwn = (today_date.year - created_date.year) * 12 + (today_date.month - created_date.month)
+
     @comments = Comment.where(:photo_id => photo_id)
+    
     render({:template => "photogram_templates/photo_info.html.erb"})
   end
 
@@ -48,5 +77,14 @@ class UsersController < ApplicationController
     photo_record.save 
 
     redirect_to("/photos/"+photo_id.to_s)
+  end
+  
+  def delete_photo 
+    photo_id = params.fetch("photo_id")
+    photo_row = Photo.where({:id => photo_id}).first 
+    comments_row = Comment.where({:photo_id => photo_id}).first
+    photo_row.destroy
+    comments_row.destroy 
+    redirect_to("/photos")
   end 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,19 @@
 Rails.application.routes.draw do
 
+get("/", {:controller => 'users', :action => 'list_users'})
 get("/users", {:controller => 'users', :action => 'list_users'})
+get("/photos", {:controller=>"users", :action=>"list_photos"})
+
 get("users/:username", {:controller => 'users', :action=>'user_info'})
-get("insert_user_record", {:controller=>"users", :action=>"create_row"})
-get("/update_username/:user_id_number", {:controller => "users", :action => "update_row"})
 get("/photos/:photo_id", {:controller => "users", :action => "photo_info"})
+
+get("insert_user_record", {:controller=>"users", :action=>"create_user_record"})
+get("insert_photo_record", {:controller=>"users", :action=>"create_photo_record"})
+get("/insert_comment", {:controller=>"users", :action=>"create_comment_record"})
+
+get("/update_username/:user_id_number", {:controller => "users", :action => "update_row"})
 get("/update_photo/:photo_id", {:controller=>"users", :action=>"update_photo"})
+get("/delete_photo/:photo_id", {:controller=>"users", :action=>"delete_photo"})
+
+
 end
